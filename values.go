499671637435ad1ca2b65939a88e1b22c2324ac7
Karan Kumar <kxenak@riseup.not>
2023-01-02 22:04:37 +0530

    Added examples of values in go.

---
+++ values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("less" + "go!")
+	fmt.Println("10 + 59 =", 10+59)
+	fmt.Println("138.0/2.0 =", 138.0/2.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
