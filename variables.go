3cdadcbc69c252061eeca80f738f2654f9b04ac8
Karan Kumar <kxenak@riseup.not>
2023-01-02 22:10:15 +0530

    Added examples of variables in go.

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str = "attackkkkkk"
+	fmt.Println(str)
+	var x, y int = 6, 9
+	fmt.Println(x, y)
+	var a int
+	fmt.Println(a)
+	hello := "not hello"
+	fmt.Println(hello)
+}
